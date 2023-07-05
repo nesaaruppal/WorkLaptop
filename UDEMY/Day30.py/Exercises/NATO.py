97cfacfcc22a40f53bbf5caa336e78069dfefee2
Nesaar Uppal <nesaaruppal@outlook.com>
2023-07-05 07:17:00 +0100

    day 300

--- UDEMY/Day30.py/Exercises/NATO.py
+++ UDEMY/Day30.py/Exercises/NATO.py
@@ -4,13 +4,30 @@ data = pandas.read_csv(r"C:\Users\NUppal\OneDrive - CBRE, Inc\CBRE\VSC\Python\UD
 
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-is_on = True
-    
-while is_on:
+
+# MY CORRECT CODE (continuous loops NOT good)
+
+#is_on = True
+  
+#while is_on:
+#    try:
+#        word = input("Enter a word: ").upper()
+#        output_list = [phonetic_dict[letter] for letter in word]
+#    except KeyError:
+#        print("Sorry, only letters from the alphabet!")
+#    else:
+#        print(output_list)
+
+# The function will only check for errors and will stop when the word contains all letters.
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
     try:
-        word = input("Enter a word: ").upper()
         output_list = [phonetic_dict[letter] for letter in word]
     except KeyError:
-        print("Sorry, only letters from the alphabet!")
+        print("Sorry, only letters in the alphabet!")
+        generate_phonetic()
     else:
         print(output_list)
+        
+generate_phonetic()
