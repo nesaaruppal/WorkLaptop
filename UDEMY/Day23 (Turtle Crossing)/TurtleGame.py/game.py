4a338841188b82935344d5dbc4236d2d0329fcd0
Nesaar Uppal <nesaaruppal@outlook.com>
2023-07-12 15:26:13 +0100

    Additions 12/07/2023

--- UDEMY/Day23 (Turtle Crossing)/TurtleGame.py/game.py
+++ UDEMY/Day23 (Turtle Crossing)/TurtleGame.py/game.py
@@ -27,6 +27,7 @@ while game_is_on:
     car_manager.create_cars()
     car_manager.move_cars()
     
+    #Detect collision with a car
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
